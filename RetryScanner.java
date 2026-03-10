dd75f1a87277144af335a414a1b8a89784cbb4d5
zeyviz <seth7negocios@gmail.com>
2026-03-10 02:56:38 -0400

    continua adicionado

--- RetryScanner.java
+++ RetryScanner.java
@@ -19,29 +19,27 @@ public class RetryScanner {
 
             System.out.println("Quantas tentativas?: ");
             int tentativas = sc.nextInt();
-            
+
             for (int i=tentativas; i > 0; i--) {
 
-            try {
-                Socket socket = new Socket();
-                socket.connect(new InetSocketAddress(ip, porta), 1000);
 
-                System.out.println("A Conexão com a Porta " + porta + " Foi estabelecida");
+                try {
+                    Socket socket = new Socket();
+                    socket.connect(new InetSocketAddress(ip, porta), 1000);
 
-                socket.close();
-                break;
-            } catch (IOException e) {
-                System.out.println("[falha na conexão] Motivo: " + e.getMessage());
+                    System.out.println("A Conexão com a Porta " + porta + " Foi estabelecida");
+
+                    socket.close();
+                    break;
+                } catch (IOException e) {
+                    System.out.println("[falha na conexão] Motivo: " + e.getMessage());
+                }
             }
-        }
 
         System.out.println("Deseja continuar? (s/n): ");
         resposta = sc.next().charAt(0);
 
         } while (resposta == 's' || resposta == 'S');
-
-
-        
         sc.close();
     }
 }
